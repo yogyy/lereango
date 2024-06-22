e780d63bf8eae9f863a4c81214b36f200e1ee09b
yogyy <yogimr261@gmail.com>
2024-06-22 21:34:15 +0700

    Structs and Interfaces

---
+++ cmd/tutorial_5/main.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+type gasEngine struct {
+	mpg     uint8
+	gallons uint8
+}
+
+type electricEngine struct {
+	mpkwh uint8
+	kwh   uint8
+}
+
+func (e gasEngine) milesLeft() uint8 {
+	return e.gallons * e.mpg
+}
+
+func (e electricEngine) milesLeft() uint8 {
+	return e.kwh * e.mpkwh
+}
+
+type engine interface {
+	milesLeft() uint8
+}
+
+func canMakeIt(e engine, miles uint8) {
+	if miles <= e.milesLeft() {
+		fmt.Println("You can make it there!")
+	} else {
+		fmt.Println("Need to fuel up first!")
+	}
+}
+func main() {
+	var myEngine gasEngine = gasEngine{25, 25}
+	var myEngine2 electricEngine = electricEngine{25, 15}
+	canMakeIt(myEngine, 50)
+	canMakeIt(myEngine2, 200)
+}
